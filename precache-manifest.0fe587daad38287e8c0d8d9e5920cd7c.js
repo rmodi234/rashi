0f52cd7c8935f84d7e7576b8046cad8ec832071a
unknown <rahul.modi@neologicx.com>
2019-06-14 22:56:21 +0530

    Updates

---
+++ precache-manifest.0fe587daad38287e8c0d8d9e5920cd7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80ebdd7f53bd4557c429fcb40c7d3cdf",
+    "url": "/rashi/index.html"
+  },
+  {
+    "revision": "608c13f7256257d9ab91",
+    "url": "/rashi/static/css/2.c7bb94bc.chunk.css"
+  },
+  {
+    "revision": "7465b608c7f4b7a17b02",
+    "url": "/rashi/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "608c13f7256257d9ab91",
+    "url": "/rashi/static/js/2.c0d1f774.chunk.js"
+  },
+  {
+    "revision": "7465b608c7f4b7a17b02",
+    "url": "/rashi/static/js/main.12f511cf.chunk.js"
+  },
+  {
+    "revision": "cbb685f3a14a3117b9e1",
+    "url": "/rashi/static/js/runtime~main.d08a6ad1.js"
+  }
+]);
